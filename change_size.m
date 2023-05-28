dede506cbacb9ab509536eafb09f5356fb9d5167
manuele09 <emacannizzo@gmail.com>
2023-05-28 22:41:57 +0200

    Added the figures of the article

--- change_size.m
+++ change_size.m
@@ -3,25 +3,25 @@ axis_font_size = 40; %x e y label dimension
 tick_font_size = 20; %axis numbers and legend dimension
 line_width = 1.5; %line width
 
-path_name = "../Figures/FiguresArticolo/InputLayer";
+path_name = "FiguresArticolo/InputLayer";
 changeSizes(path_name + "/0.fig", title_font_size, axis_font_size, tick_font_size, line_width);
 changeSizes(path_name + "/1.fig", title_font_size, axis_font_size, tick_font_size, line_width);
 changeSizes(path_name + "/2.fig", title_font_size, axis_font_size, tick_font_size, line_width);
 changeSizes(path_name + "/3.fig", title_font_size, axis_font_size, tick_font_size, line_width);
 
-path_name = "../Figures/FiguresArticolo/SumNeurons";
+path_name = "FiguresArticolo/SumNeurons";
 changeSizes(path_name + "/0.fig", title_font_size, axis_font_size, tick_font_size, line_width);
 changeSizes(path_name + "/1.fig", title_font_size, axis_font_size, tick_font_size, line_width);
 changeSizes(path_name + "/2.fig", title_font_size, axis_font_size, tick_font_size, line_width);
 changeSizes(path_name + "/3.fig", title_font_size, axis_font_size, tick_font_size, line_width);
 
-path_name = "../Figures/FiguresArticolo/MorrisLecar";
+path_name = "FiguresArticolo/MorrisLecar";
 changeSizes(path_name + "/0.fig", title_font_size, axis_font_size, tick_font_size, line_width);
 changeSizes(path_name + "/1.fig", title_font_size, axis_font_size, tick_font_size, line_width);
 changeSizes(path_name + "/2.fig", title_font_size, axis_font_size, tick_font_size, line_width);
 changeSizes(path_name + "/3.fig", title_font_size, axis_font_size, tick_font_size, line_width);
 
-path_name = "../Figures/FiguresArticolo/LiquidLayer";
+path_name = "FiguresArticolo/LiquidLayer";
 changeSizes(path_name + "/0.fig", title_font_size, axis_font_size, tick_font_size, line_width);
 changeSizes(path_name + "/1.fig", title_font_size, axis_font_size, tick_font_size, line_width);
 changeSizes(path_name + "/2.fig", title_font_size, axis_font_size, tick_font_size, line_width);
